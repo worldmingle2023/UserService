892e0c42427ef087a3e1d3cc1d0a84ccb3f5768a
heretoinfinity <mbise.engineer@gmail.com>
2023-11-24 13:16:17 -0500

    Switch to FastAPI

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
 
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return {"message": "Greetings from user service"}
-import datetime
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/")
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
+from enum import Enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
-    return render_template("index.html", times=dummy_times)
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}    
+
+@app.get("/")
+async def root():
+    return {"message": "Hello"}
 
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+
+@app.get("/users")
+async def get_all_users():
+    return {"users": "users"}
+
+@app.get("/user/{user_id}")
+async def get_all_users(user_id: int):
+    return {"user": user_id}
